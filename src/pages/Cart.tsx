5c36d4fcfb2d4af453a600f586f98e63e3334165
aha <a.halatyan@fixar.pro>
2025-07-28 17:41:05 +0300

    remove from cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -6,11 +6,17 @@ export const Cart = () => {
   const productIDs = useCartStore((state) => state.toursInCart);
   const products = tours.filter((tour) => productIDs.includes(tour.id));
 
+  const removeFromCart = useCartStore((state) => state.removeFromCart);
+
   return (
     <>
       <h1>Cart</h1>
       {products.map((tour) => (
-        <Card tour={tour} btnText="Remove from cart" btnFunction={() => {}} />
+        <Card
+          tour={tour}
+          btnText="Remove from cart"
+          btnFunction={() => removeFromCart(tour.id)}
+        />
       ))}
     </>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,10 +3,15 @@ import { create } from "zustand";
 interface CartState {
   toursInCart: number[];
   addToCart: (tourId: number) => void;
+  removeFromCart: (tourId: number) => void;
 }
 
 export const useCartStore = create<CartState>()((set) => ({
   toursInCart: [],
   addToCart: (tourId: number) =>
     set((state) => ({ toursInCart: [...state.toursInCart, tourId] })),
+  removeFromCart: (tourId: number) =>
+    set((state) => ({
+      toursInCart: state.toursInCart.filter((id) => id !== tourId),
+    })),
 }));
